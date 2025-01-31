c883d83f2b4567c4a698e069ddf86b3a6ad66a93
Patrick Leonhard <patrick.leonhard3@gmail.com>
2025-01-31 10:36:10 +0100

    8. Commit, test dependecies

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -8,5 +8,25 @@ class QueueTest extends TestCase
     public function testQueueIsEmpty(){
         $queue = new Queue();
         self::assertEquals(0, $queue->getCount());
+        return $queue;
+    }
+
+    /**
+     * @depends testQueueIsEmpty
+     */
+    public function testAnItemIsAddedToTheQueue(Queue $queue){
+        $queue->push('test');
+        self::assertEquals(1, $queue->getCount());
+        return $queue;
+    }
+
+    /**
+     * @depends testAnItemIsAddedToTheQueue
+     */
+    public function testAnItemIsRemovedFromTheQueue(Queue $queue){
+        $item = $queue->pop();
+
+        self::assertEquals(0, $queue->getCount());
+        self::assertEquals('test', $item);
     }
 }
